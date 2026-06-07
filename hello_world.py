d400fa9927b513cdb901f3c21c21ec07449b35f5
Amy McCollin <amccollinv@gmail.com>
2026-06-07 15:27:11 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# This is algorithm asks users to input their name and age, this then prints out a greeting 
+# using the name and age provided and adds the phrase "Hello Worlds!" at the end.
+# request user name
+# request user age 
+# Print phrase using name and age provided by user 
+# Print phrase "Hello Worlds!" at the end
+name = input("Enter your name:")
+age = input("Enter your age:")
+print("Hello, " + name + "!" + "you are " + age + " years old." )
+print ("Hello World!")
